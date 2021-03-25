57379c5b1ed0cc868cc0f058a7e00c0ec0821e1e
Hannehue <Hannehue@gmail.com>
2021-03-25 12:07:56 +0100

    removed fxml from gitIgnore

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("TV2-Krediteringer");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
