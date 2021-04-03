da5ba1f80ae73c6f4278ad67d3b01a657304a04e
Cazpar <casti19@student.sdu.dk>
2021-04-03 16:21:11 +0200

    Opsætning af profil sider og login
    Mangler funktionalitet og responsive layout

--- src/main/java/sample/GUIController.java
+++ src/main/java/sample/GUIController.java
@@ -3,19 +3,17 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Random;
 import java.util.ResourceBundle;
 
 public class GUIController implements Initializable {
 
     @FXML
     protected void handleLogin(ActionEvent Event) throws IOException {
+        //redirect til Login side
         Main.setRoot("Login");
     }
     @FXML
@@ -30,12 +28,24 @@ public class GUIController implements Initializable {
     }
     @FXML
     protected  void handleMyProfile(ActionEvent Event) throws IOException{
-        //Tilføj kode der faktisk logger ud fra en konto
+        //Går til profilen af den person der er logget ind
+        Main.setRoot("Profile");
+    }
+    @FXML
+    protected  void handleEditProfile(ActionEvent Event) throws IOException{
+        //Tilføj kode der henter brugerens informationer og sætter dem i textfields i stedet for labels
+        Main.setRoot("Profile-edit");
+    }
+    @FXML
+    protected  void handleSaveProfile(ActionEvent Event) throws IOException{
+        //Tilføj kode der tager det information brugeren har indtastet i textfields og overskriv brugerens gamle informationer i json-fil
         Main.setRoot("Profile");
     }
 
     @FXML
     protected void ToFrontpage(MouseEvent Event) throws IOException {
+        //denne bruges på billedet af TV 2
+        //hver gang billedet klikkes ryges der til forsiden
         Main.setRoot("GUI");
     }
 
