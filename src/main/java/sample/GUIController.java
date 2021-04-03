143bf0bc666f8b3f1a67e5c874412f45571aac4d
Cazpar <casti19@student.sdu.dk>
2021-04-03 15:53:46 +0200

    Opsætning af gui skelet

--- src/main/java/sample/GUIController.java
+++ src/main/java/sample/GUIController.java
@@ -1,5 +1,47 @@
 package sample;
 
-public class GUIController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class GUIController implements Initializable {
+
+    @FXML
+    protected void handleLogin(ActionEvent Event) throws IOException {
+        Main.setRoot("Login");
+    }
+    @FXML
+    protected  void handleLoggingIn(ActionEvent Event) throws IOException{
+        //Tilføj kode der faktisk logger ind på en konto
+        Main.setRoot("LoggedIn");
+    }
+    @FXML
+    protected  void handleLogout(ActionEvent Event) throws IOException{
+        //Tilføj kode der faktisk logger ud fra en konto
+        Main.setRoot("GUI");
+    }
+    @FXML
+    protected  void handleMyProfile(ActionEvent Event) throws IOException{
+        //Tilføj kode der faktisk logger ud fra en konto
+        Main.setRoot("Profile");
+    }
+
+    @FXML
+    protected void ToFrontpage(MouseEvent Event) throws IOException {
+        Main.setRoot("GUI");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,16 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.IOException;
 
+public class Main extends Application {
+    private static Scene scene;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("GUI.fxml"));
+        scene = new Scene(loadFXML("GUI"), 960,720);
         primaryStage.setTitle("TV2-Krediteringer");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    //sætter root for scenen, så den ved hvilken fil der skal vises
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+    //metode til at indlæse den nye .fxml fil som skal vises
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getClassLoader().getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 
     public static void main(String[] args) {
         launch(args);
