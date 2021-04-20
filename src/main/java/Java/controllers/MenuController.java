a1a6d31012587b3dfcb53200d315ba8838c9085a
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-04-20 13:25:26 +0200

    added personal profiladded personal profilee

--- src/main/java/Java/controllers/MenuController.java
+++ src/main/java/Java/controllers/MenuController.java
@@ -138,15 +138,10 @@ public class MenuController implements Initializable {
     public void handlePersonalProfile(MouseEvent mouseEvent) {
         try {
             setContentPane("PersonalProfile.fxml");
+            hideMenu();
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    //Credit Person
-    @FXML
-    protected  void handleEditProfile(ActionEvent Event) throws IOException{
-        //Tilføj kode der henter brugerens informationer og sætter dem i textfields i stedet for labels
-        CreditSystemController.setRoot("CreditPersonProfile-edit");
-    }
 }
