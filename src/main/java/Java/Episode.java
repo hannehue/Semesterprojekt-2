1e6a0d39a0ce9ec0738baa6ee9f35dc75fe82a55
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-04-20 10:19:32 +0200

    getting and setting roles in movie

--- src/main/java/Java/Episode.java
+++ src/main/java/Java/Episode.java
@@ -1,12 +1,17 @@
 package Java;
 
 import java.util.Date;
+import java.util.HashMap;
 
 public class Episode extends Production{
 
     private int seasonID;
+    private HashMap<String, Person> cast;
+    private HashMap<Role, Person[]> productionTeam;
 
-    public Episode(String title, Date dateAdded, int seasonID, int creditID, boolean approved, String description, int productionID, int lengthInSecs, Date releaseDate){
+    public Episode(String title, Date dateAdded, int seasonID, int creditID,
+                   boolean approved, String description, int productionID,
+                   int lengthInSecs, Date releaseDate){
         super(title, dateAdded, creditID, approved, description, productionID, lengthInSecs, releaseDate);
         this.seasonID = seasonID;
     }
