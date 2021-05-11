4ad1b0dcc21058203ec79bbdda37344453980be6
Cazpar <Casti19@student.sdu.dk>
2021-05-11 11:06:55 +0200

    PutInDatabase metode overload

--- src/main/java/Java/data/DatabaseLoaderFacade.java
+++ src/main/java/Java/data/DatabaseLoaderFacade.java
@@ -1,4 +1,52 @@
 package Java.data;
 
+import Java.interfaces.*;
+
 public class DatabaseLoaderFacade {
+    //Metoder som tager en parameter i forhold til hvad der skal sendes til databsen
+    public void putInDatabase() {
+
+    }
+    //---------------------------------------------------------
+    //               Put Person or company
+    //---------------------------------------------------------
+    public void putInDatabase(IPerson person){
+        //Ijob følger med her, derfor er der ikke en putInDatabase(IJob)
+        //charactername er givet i IJob
+    }
+    public void putInDatabase(ICompany company){
+
+    }
+    //---------------------------------------------------------
+    //                  Put Credit
+    //---------------------------------------------------------
+    public void putInDatabase(ICredit credit) {
+
+    }
+    //---------------------------------------------------------
+    //                   Put Programs
+    //---------------------------------------------------------
+    public void putInDatabase(IEpisode episode) {
+
+    }
+    public void putInDatabase(ISeason season) {
+
+    }
+    public void putInDatabase(IShow show) {
+
+    }
+    public void putInDatabase(IMovie movie){
+
+    }
+    //---------------------------------------------------------
+    //                  Put Production
+    //---------------------------------------------------------
+    public void putInDatabase(IProduction production) {
+
+    }
+
+    //---------------------------------------------------------
+    //                  Get Production
+    //---------------------------------------------------------
+    
 }
